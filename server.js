e2e93fa4f8f7bae5afa3f08f3a0e301a2e125e3d
Dario Vladovic <d.vladimyr@gmail.com>
2015-09-14 07:48:34 +0200

    Setting ip and port as application properties - adapting to OpenShift requirements

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ var express = require('express'),
     cors    = require('cors'),
     app     = express();
 
-var argv = require('minimist')(process.argv.slice(2)),
-    port = argv.p || argv.port || process.env.OPENSHIFT_NODEJS_PORT || 4747,
-    host = '0.0.0.0' || process.env.OPENSHIFT_NODEJS_IP;
+var argv = require('minimist')(process.argv.slice(2));
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || argv.p || argv.port || 4747);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 
 app.use(cors());
 
 var annotationsApi = require('./annotations-api.js');
 app.use('/services/annotations', annotationsApi);
 
-var server = app.listen(port, host, function() {
+var server = app.listen(app.get('port'), app.get('ip'), function() {
     var serverAddr = server.address();
 
     console.log(util.format(
